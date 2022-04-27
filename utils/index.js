d083c9ac24a7b0826c1c99ede8e7b6db5246ff13
InaiaKenia <inaiakenia15@gmail.com>
2022-04-26 22:01:29 -0600

    Started index.js const/let, classes imported

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,23 @@
-const Manager = require('./library/Manager')
-const Employee = require('./library/Employee')
-const Engineer = require('./library/Engineer')
-const Intern = require('./library/Intern')
+const inquirer = require("inquirer");
+
+const fs = require ("fs");
+// create a markdown
+const generateHTML = require("./generateHTML")
+// imported all classes
+const Manager = require('../library/Manager')
+const Employee = require('../library/Employee')
+const Engineer = require('../library/Engineer')
+const Intern = require('../library/Intern')
+
+
+// prevents the user from empty string for any question.
+const validateInput = (userInput) => {
+    if (userInput === ""){
+        return "Please type your answer.";
+    }else {
+        return true;
+    };
+}
 
 // create a function for each one
 
