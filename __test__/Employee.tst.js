4f741b437300d6d6a8150ecbb0796936d49f49ec
InaiaKenia <inaiakenia15@gmail.com>
2022-04-28 21:24:22 -0600

    employee test

--- __test__/Employee.tst.js
+++ __test__/Employee.tst.js
@@ -1,6 +1,39 @@
-const Letter = require('../lib/Letter');
+const { TestWatcher } = require('jest');
+const Employee = require('../library/Employee');
 
-describe('Visibility', () => {
-  it('Characters that are digits or letters should not be visible', () => {
-    expect(new Letter('j').visible).toBe(false);
+describe('Employee', () => {
+  const mockEmployee = {
+    name: "Inaia",
+    id: 30,
+    email: "inaia5martin@gmail.com",
+  };
+  describe("constructor tests", () => {
+    test("should construct a new instance of an employee class", () => {
+      const employee = new Employee(mockEmployee);
+      expect(employee).toBeInstanceOf(Employee);
+    });
+    test("should construct a new instance of an employee class with name,ide,email", () => {
+      const employee = new Employee(mockEmployee);
+      expect(employee).toEqual({
+        name: "Inaia",
+        id: 30,
+        email: "inaia5martin@gmail.com",
+      });
+    });
   });
+describe("method tests",()=> {
+  test("should return id when the getId method is called",()=>{
+    const employee=new Employee(mockEmployee);
+    expect(employee.getId()).toEqual(30);
+  });
+
+  test ("should return name when the getName method is called",()=>{
+    const employee=new Employee(mockEmployee);
+    expect(employee.getName()).toEqual("Inaia");
+  });
+  test ("should return email when the getEmail method is called",()=>{
+    const employee=new Employee(mockEmployee);
+    expect(employee.getEmail()).toEqual("inaia5martin@gmail.com");
+});
+});
+});
