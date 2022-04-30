b97c941532467160478499d5d97b5421b67f28b2
InaiaKenia <inaiakenia15@gmail.com>
2022-04-30 14:43:40 -0600

    fixed index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const inquirer =require("inquirer");
+const inquirer = require("inquirer");
 
 const fs = require("fs");
 // create a markdown
-const generateHTML = require("./utils/generateHTML")
+const generateHTMLTemplate = require("./utils/generateHTML")
 // imported all classes
 const Manager = require('./library/Manager')
 const Employee = require('./library/Employee')
 const Engineer = require('./library/Engineer')
-const Intern = require('./library/Intern')
-
+const Intern = require('./library/Intern');
+const { start } = require("repl");
+let employeeDB=[]
 
 // prevents the user from empty string for any question.
 const validateInput = (userInput) => {
@@ -20,116 +21,120 @@ const validateInput = (userInput) => {
 }
 
 const init = async () => {
-    await createManager();
-    while (!isTeamComplete) {
-        const employeeTypeQuestion = [
-            {
-                type: "list",
-                message: "Select the employee type:",
-                name: "employeeType",
-                choices: [
-                    { name: "Engineer", value: "engineer", short: "Engineer" },
-                    { name: "Intern", value: "intern", short: "Intern" },
-                    { name: "None", value: "none", short: "None" },
-                ],
-            },
-        ]
-    }
+ 
+
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "Select the employee type:",
+            name: "employeeType",
+            choices: [
+                { name: "Engineer", value: "1", short: "Engineer" },
+                { name: "Intern", value: "2", short: "Intern" },
+                { name: "None", value: "3", short: "None" },
+            ],
+        },
+    ]).then(employee => {
+        switch (employee.employeeType) {
+            case "1":
+                createEngineer();
+                break;
+            case "2":
+                createIntern();
+                break;
+            case "3":
+                generateHTML();
+
+        }
+    })
+
 }
 // employee object will be generated
-const employeeType = await inquirer.prompt(employeeTypeQuestion); // should I keep or delete await ????
+// const employeeType = await inquirer.prompt(employeeTypeQuestion); // should I keep or delete await ????
 
-if (employeeType === "none") {
-    isTeamComplete = true;
-} else {
-    if (employeeType === "engineer") {
-        await createEngineer();
-    }
-    if (employeeType === "intern") {
-        await createIntern();
-    }
-}
+
 
 //employees array + any erros with writing to the file.
-const HTML = generateHTML(employee);
-fs.writeFileSync("team profile.html",HTML,(err)=> {
-    if (err){
+const  generateHTML =(employee) =>{
+fs.writeFileSync("team profile.html", HTML, (err) => {
+    if (err) {
         console.log(err);
-    }else{
+    } else {
         console.log("HTML created");
     }
 })
-
+}
 // manager function + array of questions
-const createManager = async () =>{
-    const managerQuestions =[
+const createManager = async () => {
+    const managerQuestions = [
         {
-            type:"input",
-            message:"Enter manager name:",
-            name:"name",
-            validate:validateInput,
+            type: "input",
+            message: "Enter manager name:",
+            name: "name",
+            validate: validateInput,
         },
         {
-            type:"input",
-            message:"Enter employee ID:",
-            name:"id",
-            validate:validateInput,
+            type: "input",
+            message: "Enter employee ID:",
+            name: "id",
+            validate: validateInput,
         },
         {
-            type:"input",
-            message:"Enter your office number:",
-            name:"officeNumber",
-            validate:validateInput,
+            type: "input",
+            message: "Enter your office number:",
+            name: "officeNumber",
+            validate: validateInput,
         },
 
         {
-            type:"input",
-            message:"Enter work email:",
-            name:"email",
-            validate:validateInput,
+            type: "input",
+            message: "Enter work email:",
+            name: "email",
+            validate: validateInput,
 
         },
     ];
 
-const managerAnswers = await inquirer.prompt(managerQuestions);
-const manager = new Manager(managerAnswers);
-employee.push(manager);
+    const managerAnswers = await inquirer.prompt(managerQuestions);
+    const manager = new Manager(managerAnswers);
+    employeeDB.push(manager);
 };
 
 // engineer function + array of questions
 const createEngineer = async () => {
     const engineerQuestions = [
         {
-            type:"input",
-            message:"Enter engineer name:",
-            name:"name",
-            validate:validateInput,
+            type: "input",
+            message: "Enter engineer name:",
+            name: "name",
+            validate: validateInput,
         },
         {
-            type:"input",
-            message:"Enter engineer ID:",
-            name:"id",
-            validate:validateInput,
+            type: "input",
+            message: "Enter engineer ID:",
+            name: "id",
+            validate: validateInput,
         },
         {
-            type:"input",
-            message:"Enter engineer email:",
-            name:"email",
-            validate:validateInput,
+            type: "input",
+            message: "Enter engineer email:",
+            name: "email",
+            validate: validateInput,
         },
 
         {
-            type:"input",
-            message:"Enter engineer github profile:",
-            name:"github",
-            validate:validateInput,
+            type: "input",
+            message: "Enter engineer github profile:",
+            name: "github",
+            validate: validateInput,
 
         },
     ];
 
     const engineerAnswers = await inquirer.prompt(engineerQuestions);
     const engineer = new Engineer(engineerAnswers);
-    employee.push(engineer);
+    employeeDB.push(engineer);
+    init()
 };
 
 
@@ -137,42 +142,48 @@ const createEngineer = async () => {
 const createIntern = async () => {
     const internQuestions = [
         {
-            type:"input",
-            message:"Enter intern name:",
-            name:"name",
-            validate:validateInput,
+            type: "input",
+            message: "Enter intern name:",
+            name: "name",
+            validate: validateInput,
         },
         {
-            type:"input",
-            message:"Enter intern ID:",
-            name:"id",
-            validate:validateInput,
+            type: "input",
+            message: "Enter intern ID:",
+            name: "id",
+            validate: validateInput,
         },
         {
-            type:"input",
-            message:"Enter intern email:",
-            name:"email",
-            validate:validateInput,
+            type: "input",
+            message: "Enter intern email:",
+            name: "email",
+            validate: validateInput,
         },
 
         {
-            type:"input",
-            message:"Enter intern school name:",
-            name:"school",
-            validate:validateInput,
+            type: "input",
+            message: "Enter intern school name:",
+            name: "school",
+            validate: validateInput,
 
         },
     ];
 
     const internAnswers = await inquirer.prompt(internQuestions);
     const intern = new Intern(internAnswers);
-    employee.push(intern);
+    employeeDB.push(intern);
+    init()
 };
 
 // function call to start app
 
-init();
+const startApp = async () => {
+    await createManager();
+    init(
+
+    )
+}
 
 
 
-
+startApp()
